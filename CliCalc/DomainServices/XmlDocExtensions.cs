2f5781d7e82ab90a6564813658d7c0cc384b8fbb
webmaster442 <ruzsinszki.gabor@gmail.com>
2024-11-26 18:18:53 +0100

    Documentation for functions

---
+++ CliCalc/DomainServices/XmlDocExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+using CliCalc.Domain.XmlDoc;
+
+namespace CliCalc.DomainServices;
+internal static partial class XmlDocExtensions
+{
+    [GeneratedRegex(@"M\:CliCalc\.Functions\.Global\.(.+)\(")]
+    private static partial Regex MethodNameRegex(); 
+
+    public static bool IsFunction(this DocMember member)
+        => member.Name.StartsWith("M:");
+
+    public static string GetName(this DocMember member)
+    {
+        var parts = MethodNameRegex().Split(member.Name);
+        return parts[1];
+    }
+
+    public static string GetDocumentation(this DocMember member)
+    {
+        return member.Summary;
+    }
+}
--- CliCalc/Engine/EnginePromptCallbacks.cs
+++ CliCalc/Engine/EnginePromptCallbacks.cs
@@ -2,53 +2,66 @@
 using System.Xml.Serialization;
 
 using CliCalc.Domain.XmlDoc;
+using CliCalc.DomainServices;
 using CliCalc.Interfaces;
 
 using PrettyPrompt;
 using PrettyPrompt.Completion;
 using PrettyPrompt.Consoles;
 using PrettyPrompt.Documents;
+using PrettyPrompt.Highlighting;
 
 namespace CliCalc.Engine;
 
 internal sealed class EnginePromptCallbacks : PromptCallbacks
 {
-    private readonly XmlDoc _xmlDoc;
-    private readonly Dictionary<string, string> _hashMarks = new();
+    private readonly Dictionary<string, string> _hashMarks;
+    private readonly Dictionary<string, string> _functions;
+    private readonly ImmutableArray<CharacterSetModificationRule> _hasmarkRules;
+    private readonly ImmutableArray<CharacterSetModificationRule> _functionRules;
 
     public EnginePromptCallbacks(IReadOnlyDictionary<string, IHashMarkCommand> commands)
     {
         XmlSerializer serializer = new XmlSerializer(typeof(XmlDoc), new XmlRootAttribute("doc"));
         var file = Path.Combine(AppContext.BaseDirectory, "CliCalc.Functions.xml");
+        XmlDoc document;
         using (var stream = File.OpenRead(file))
         {
-            _xmlDoc = serializer.Deserialize(stream) as XmlDoc
+            document = serializer.Deserialize(stream) as XmlDoc
                 ?? new XmlDoc();
         }
-        foreach (var command in commands)
-        {
-            _hashMarks.Add(command.Key.Substring(1), command.Value.Description);
-        }
+        _functions = document.Members
+            .Where(m => m.IsFunction())
+            .ToDictionary(m => m.GetName(), m => m.GetDocumentation());
+
+        _hashMarks = commands.ToDictionary(x => x.Key.Substring(1), x => x.Value.Description);
+        _hasmarkRules = new[] { new CharacterSetModificationRule(CharacterSetModificationKind.Remove, new[] { '#' }.ToImmutableArray()) }.ToImmutableArray();
+        _functionRules = new[] { new CharacterSetModificationRule(CharacterSetModificationKind.Add, new[] { ' ' }.ToImmutableArray()) }.ToImmutableArray();
     }
 
-    protected override Task<IReadOnlyList<CompletionItem>> GetCompletionItemsAsync(string text, int caret, TextSpan spanToBeReplaced, CancellationToken cancellationToken)
+    protected override Task<IReadOnlyList<CompletionItem>> GetCompletionItemsAsync(string text,
+                                                                                   int caret,
+                                                                                   TextSpan spanToBeReplaced,
+                                                                                   CancellationToken cancellationToken)
     {
         var typedWord = text.AsSpan(spanToBeReplaced.Start, spanToBeReplaced.Length).ToString();
+        return text.StartsWith('#')
+            ? ItemsFromDictionary(typedWord, _hashMarks, _hasmarkRules)
+            : ItemsFromDictionary(typedWord, _functions, _functionRules);
+    }
 
+    private static Task<IReadOnlyList<CompletionItem>> ItemsFromDictionary(string typedWord,
+                                                                           Dictionary<string, string> hashMarks,
+                                                                           ImmutableArray<CharacterSetModificationRule> rules)
+    {
+        var list = hashMarks
+            .Where(x => x.Key.StartsWith(typedWord, StringComparison.OrdinalIgnoreCase))
+            .Select(x => new CompletionItem(replacementText: x.Key,
+                                            displayText: x.Key,
+                                            getExtendedDescription: (ct) => Task.FromResult(new FormattedString(x.Value)),
+                                            commitCharacterRules: rules))
+            .ToList();
 
-        if (text.StartsWith('#'))
-        {
-
-            return Task.FromResult<IReadOnlyList<CompletionItem>>(_hashMarks
-                .Where(x => x.Key.StartsWith(typedWord, StringComparison.OrdinalIgnoreCase))
-                .Select(x => new CompletionItem(replacementText: x.Key,
-                                                displayText: x.Value,
-                                                commitCharacterRules: new[] { new CharacterSetModificationRule(CharacterSetModificationKind.Remove, new[] { '#' }.ToImmutableArray()) }.ToImmutableArray()))
-                .ToList());
-        }
-        else
-        {
-            return Task.FromResult<IReadOnlyList<CompletionItem>>(new List<CompletionItem>());
-        }
+        return Task.FromResult<IReadOnlyList<CompletionItem>>(list);
     }
 }

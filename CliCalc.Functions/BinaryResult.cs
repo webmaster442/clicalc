d20f11c435dcf7ff2c1846d24545ffc895270c94
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2024-12-07 11:45:32 +0100

    Binary result type

---
+++ CliCalc.Functions/BinaryResult.cs
@@ -0,0 +1,27 @@
+﻿namespace CliCalc.Functions;
+
+/// <summary>
+/// Represent a binary result.
+/// </summary>
+public sealed class BinaryResult
+{
+    private readonly byte[] _data;
+
+    /// <summary>
+    /// Creates a new instance form a long result
+    /// </summary>
+    /// <param name="data">the data source long</param>
+    public BinaryResult(long data)
+    {
+        _data = BitConverter.GetBytes(data);
+    }
+
+    /// <summary>
+    /// Cereates a new instance from a double result
+    /// </summary>
+    /// <param name="data">the data source double</param>
+    public BinaryResult(double data)
+    {
+        _data = BitConverter.GetBytes(data);
+    }
+}

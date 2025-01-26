19d8aeeb6e677ba6d84b2369d73bdf41f84de5ba
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2025-01-26 17:17:22 +0100

    Web command

---
+++ CliCalc/HashMarkCommands/web.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+using CliCalc.Domain;
+using CliCalc.Engine;
+using CliCalc.Interfaces;
+
+using Spectre.Console;
+
+namespace CliCalc.HashMarkCommands;
+
+internal class Web : IHashMarkCommand
+{
+    public string Name => "web";
+
+    public string Description => "Open program website";
+
+    public Task<HashMarkResult> ExecuteAsync(Arguments args, IAnsiConsole ansiConsole, IMediator mediator, CancellationToken cancellationToken)
+    {
+        using var process = new Process();
+        process.StartInfo.FileName = "https://github.com/webmaster442/clicalc";
+        process.StartInfo.UseShellExecute = true;
+        process.Start();
+        return Task.FromResult(new HashMarkResult());
+    }
+}

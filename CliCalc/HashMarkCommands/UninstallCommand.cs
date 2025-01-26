ccd4cdcba0b0c258900a9d8bfc67eb2b54769897
Ruzsinszki Gábor <ruzsinszki.gabor@gmail.com>
2025-01-26 17:08:13 +0100

    Uninstall command

---
+++ CliCalc/HashMarkCommands/UninstallCommand.cs
@@ -0,0 +1,29 @@
+﻿using CliCalc.Domain;
+using CliCalc.Engine;
+using CliCalc.Interfaces;
+
+using Spectre.Console;
+
+using Webmaster442.WindowsTerminal;
+
+namespace CliCalc.HashMarkCommands;
+
+internal class UninstallCommand : IHashMarkCommand
+{
+    public string Name => "uninstall";
+
+    public string Description => "UnInstall calculator from Windows terminal";
+
+    public Task<HashMarkResult> ExecuteAsync(Arguments args, IAnsiConsole ansiConsole, IMediator mediator, CancellationToken cancellationToken)
+    {
+        if (!WindowsTerminal.FragmentExtensions.IsFragmentInstalled(InstallCommand.AppName, InstallCommand.File))
+            return Task.FromResult(new HashMarkResult("Calculator is already installed"));
+
+        bool result = WindowsTerminal.FragmentExtensions.TryRemoveFragment(InstallCommand.AppName, InstallCommand.File);
+
+        if (!result)
+            return Task.FromResult(new HashMarkResult("Failed to uninstall calculator"));
+
+        return Task.FromResult(new HashMarkResult());
+    }
+}
